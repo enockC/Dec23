5a6c7d690b39159de101db39e6d537e7202d8f65
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2019-12-23 16:04:53 +0200

    Add project files.

---
+++ Dec23/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dec23
+{
+    public class Class1
+    {
+        public void Print()
+        {
+
+        }
+    }
+}
---
+++ Dec23Tests/Class1Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dec23;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dec23.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace Dec23Tests
+{
+    class Class1Tests
+    {
+    }
+}
